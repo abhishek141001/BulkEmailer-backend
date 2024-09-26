c362b62a5630f1b3bc4dd31d483ef7e42cd1d916
abhishek <ojhaabhishekraj14@gmail.com>
2024-09-26 08:37:42 +0530

    ..

--- modal/Task.js
+++ modal/Task.js
@@ -1,19 +1,14 @@
-// models/Task.js
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
-  task: { type: String, required: true },
-  description: { type: String },
-  problem: { type: String },
-  media: {
-    filename: String,
-    path: String,
-    mimetype: String,
-    size: Number,
-  },
-  createdAt: { type: Date, default: Date.now },
+    task: String,
+    description: String,
+    problem: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Task = mongoose.model('Task', taskSchema);
-
 export default Task;
